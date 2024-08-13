9775413d56d7359ff9eac1acfdec8270f6a87c10
belozerov_ae <belozerov_ae@resanta.ru>
2024-08-13 16:04:54 +0300

    fix rotate animation, fix useEffect from stop rotate

--- src/pages/mining/widgets/Generator/Generator.jsx
+++ src/pages/mining/widgets/Generator/Generator.jsx
@@ -13,7 +13,7 @@ const Generator = ({ onClick }) => {
   const [isRotating, setIsRotating] = useState(false);
   const speedRef = useRef(null);
   const maxSpeed = 32;
-  const acceleration = 0.1;
+  const acceleration = 0.3;
 
   const rotateFunc = () => {
     const rotate = () => {
@@ -22,26 +22,26 @@ const Generator = ({ onClick }) => {
           speedRef.current += acceleration;
           speedRef.current > maxSpeed && (speedRef.current = maxSpeed);
         } else {
-          speedRef.current -= acceleration;
+          speedRef.current -= acceleration * 2;
           speedRef.current <= 0 && (speedRef.current = 0);
         }
 
         rotation.current += speedRef.current;
         genRef.current.style.transform = `rotate(${rotation.current}deg)`;
-        speedRef.current === 0 && setIsRotating(false);
 
         animationRef.current = requestAnimationFrame(rotate); // Запрос следующего кадра
+        speedRef.current !== 0 ? setIsRotating(true) : setIsRotating(false);
       }
     };
-
     animationRef.current = requestAnimationFrame(rotate); // Запуск анимации
-    setIsRotating(true);
   };
 
   const rotateFuncMemo = useCallback(rotateFunc, []); // Мемоизация функции
 
   useEffect(() => {
     if (!isRotating) {
+      console.log("стоит!");
+
       setCountOfRotate(0);
       cancelAnimationFrame(animationRef.current);
     }
@@ -67,11 +67,9 @@ const Generator = ({ onClick }) => {
   useEffect(() => {
     if (!isImgLoading && genRef.current) {
       const newPositions = pointRef.current.map(() => ({
-        top:
-          rotateContainerBounding?.y * 1.1 -
-          (Math.floor(Math.random() * 50) + 1),
+        top: 70 - (Math.floor(Math.random() * 50) + 1),
         left:
-          rotateContainerBounding?.x / 1.5 +
+          rotateContainerBounding?.x * 3 +
           (Math.floor(Math.random() * (2 * 150 + 1)) - 150),
       }));
       setPositions(newPositions);
@@ -117,65 +115,69 @@ const Generator = ({ onClick }) => {
   }, [countOfRotate, rotateFuncMemo]);
 
   return (
-    <div className="generator">
-      <div className=" generator__level level">
-        <div className="level__info">
-          <TextString
-            firstSmall={"Уровень"}
-            big={1}
-            bigFontSize={"15px"}
-            bigTextMargin={"0px 0px 0px 5px"}
-          />
-          <HelpBtn onClick={onClick} />
-        </div>
-        <UpBtn onClick={onClick} />
+    <>
+      <div className="poin-container">
+        {pointRef.current.map((ref, index) => {
+          return (
+            <div
+              style={{
+                top: `${positions[index]?.top}px`,
+                left: `${positions[index]?.left}px`,
+              }}
+              key={index}
+              ref={ref}
+              className={`${
+                index < countOfRotate % pointRef.current.length
+                  ? "generator-point"
+                  : "generator-point_invisible"
+              }`}
+            >
+              +1
+            </div>
+          );
+        })}
       </div>
-      <div
-        ref={rotateContainerRef}
-        onClick={handleClick}
-        className="geterator__rotateContainer"
-      >
-        <img
-          ref={genRef}
-          onClick={handleClick}
-          className="geterator__img"
-          src="./images/generatorFromRotate.png"
-          alt="generator"
-        />
-      </div>
-      {pointRef.current.map((ref, index) => {
-        return (
-          <div
-            style={{
-              top: `${positions[index]?.top}px`,
-              left: `calc(40vw + ${positions[index]?.left}px)`,
-            }}
-            key={index}
-            ref={ref}
-            className={`${
-              index < countOfRotate % pointRef.current.length
-                ? "generator-point"
-                : "generator-point_invisible"
-            }`}
-          >
-            +1
+      <div className="generator">
+        <div className=" generator__level level">
+          <div className="level__info">
+            <TextString
+              firstSmall={"Уровень"}
+              big={1}
+              bigFontSize={"15px"}
+              bigTextMargin={"0px 0px 0px 5px"}
+            />
+            <HelpBtn onClick={onClick} />
           </div>
-        );
-      })}
-      <div className="generator__power level__info">
-        <img
-          className="generator__lightning lightning-with-back"
-          src="./images/lightningWithBackground.png"
-          alt="lightning"
-        />
-        <TextString
-          big={[5000, 5000]}
-          secondSmall={"Вт•Ч"}
-          bigFontSize={"15px"}
-          bigTextMargin={"0px 5px 0px 10px"}
-        />
+          <UpBtn onClick={onClick} />
+        </div>
+        <div
+          ref={rotateContainerRef}
+          onClick={handleClick}
+          className="geterator__rotateContainer"
+        >
+          <img
+            ref={genRef}
+            // onClick={handleClick}
+            className="geterator__img"
+            src="./images/generatorFromRotate.png"
+            alt="generator"
+          />
+        </div>
+        <div className="generator__power level__info">
+          <img
+            className="generator__lightning lightning-with-back"
+            src="./images/lightningWithBackground.png"
+            alt="lightning"
+          />
+          <TextString
+            big={[5000, 5000]}
+            secondSmall={"Вт•Ч"}
+            bigFontSize={"15px"}
+            bigTextMargin={"0px 5px 0px 10px"}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
